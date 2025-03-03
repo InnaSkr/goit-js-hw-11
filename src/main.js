adc13ff2d0a94581a287b5c6decabc0fc687be38
Inna <skripnik13@gmail.com>
2025-03-03 19:16:29 +0200

    add indx file

--- src/main.js
+++ src/main.js
@@ -1,35 +1,40 @@
-import { fetchImages } from './js/pixabay-api.js';
-import { renderImages } from './js/render-functions.js';
+import { getImage } from './js/pixabay-api';
+import { galleryMarkup } from './js/render-functions';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import errorIcon from './img/error.svg';
 
-const form = document.querySelector('#search-form');
-const loader = document.querySelector('.loader');
-const gallery = document.querySelector('.gallery');
+const form = document.querySelector('.form');
 
-form.addEventListener('submit', async (event) => {
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
     event.preventDefault();
-    const query = event.target.elements.searchQuery.value.trim();
-    
-    if (!query) {
-        iziToast.warning({ title: 'Warning', message: 'Please enter a search term!' });
+
+    const input = document.querySelector('.user-input').value.trim();
+    const gallery = document.querySelector('.gallery');
+
+    if (input === '') {
         return;
     }
-    
-    gallery.innerHTML = '';
-    loader.style.display = 'block';
-    
-    try {
-        const images = await fetchImages(query);
-        if (images.length === 0) {
-            iziToast.error({ title: 'Error', message: 'Sorry, there are no images matching your search query. Please try again!' });
-        } else {
-            renderImages(images);
-        }
-    } catch (error) {
-        iziToast.error({ title: 'Error', message: 'Something went wrong. Please try again later.' });
-    }
-    
-    loader.style.display = 'none';
-    form.reset();
-});
+
+    gallery.innerHTML = '<p>Loading images, please wait...</p><span class="loader"></span>';
+
+    getImage(input)
+        .then(response => galleryMarkup(response))
+        .catch(error => {
+            iziToast.show({
+                messageColor: '#FAFAFB',
+                messageSize: '16px',
+                backgroundColor: '#EF4040',
+                iconUrl: errorIcon,
+                transitionIn: 'bounceInLeft',
+                position: 'topRight',
+                displayMode: 'replace',
+                closeOnClick: true,
+                message: 'Something went wrong. Please, try again.',
+            });
+            gallery.innerHTML = '';
+            console.log(error);
+        });
+}

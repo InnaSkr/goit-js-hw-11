4a9475b41e18be8cc67957fd569dd0771005f166
Inna <skripnik13@gmail.com>
2025-03-26 21:02:48 +0200

    add index file

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,20 +3,22 @@ import axios from 'axios';
 const API_KEY = '49113920-f9af0cc3a936c3f1f6e808981';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
-    try {
-        const response = await axios.get(BASE_URL, {
-            params: {
-                key: API_KEY,
-                q: query,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: true,
-            },
-        });
-        return response.data.hits;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        throw error;
-    }
+export function fetchPixabay(searchQuery) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      throw error;
+    });
 }
